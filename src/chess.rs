347d2218f1faecd354cd8b10491bf0136b981f6a
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2022-06-04 22:43:14 -0500

    Main abstraction work done (for practice, plus it panics too)

---
+++ src/chess.rs
@@ -0,0 +1,135 @@
+use regex::{Regex, Captures};
+use std::{fmt, ops::BitAnd};
+
+pub struct Position {
+    pub rank: u8,
+    pub file: char
+}
+
+// Note to self: basically the to_string of Rust
+// (Rust actually has a ToString trait, but Display implements ToString)
+impl fmt::Display for Position {
+   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+       write!(f, "{}{}", self.file, self.rank)
+   } 
+}
+
+pub enum Player {
+    WHITE,
+    BLACK
+}
+
+impl fmt::Display for Player {
+   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+       match self {
+           Player::WHITE => write!(f, "White"),
+           Player::BLACK => write!(f, "Black")
+       }
+   } 
+}
+
+#[repr(u8)]
+pub enum CastlingAvail {
+    WhiteKingside = 1,
+    WhiteQueenside = 2,
+    BlackKingside = 4,
+    BlackQueenside = 8
+}
+
+// Makes it easier to bitwise and to find which castling moves are available
+impl BitAnd<CastlingAvail> for u8 {
+    fn bitand(self, rhs: CastlingAvail) -> u8 {
+        self & (rhs as u8)
+    }
+    type Output = u8;
+}
+
+pub struct ChessState {
+    pub board: String,
+    pub active_player: Player,
+    pub castling_avail: u8,
+    pub en_passant: Position,
+    pub halfmove_clock: u8,
+    pub fullmoves: u8
+}
+
+impl ChessState {
+    pub fn new(fen: &String) -> ChessState {
+        let mut new_board : String = String::new();
+
+        // step 0: regex to split the sections up
+        let re : Regex = Regex::new(r"[\w/-]+").unwrap();
+        // still not understanding the whole borrow thing, I guess it's due to being immutable?
+        let sections : Vec<&str> = re.captures_iter(&fen)
+            .map(|x: Captures| x.get(0).unwrap().as_str()) // Also map into string type
+            .collect();
+
+        // step 1: get boardstate
+        for c in sections[0].chars() {
+            if c == '/' {
+               new_board.push('\n');
+            } else if c.is_numeric() {
+               // Really wierd thing? Doesn't compile unless the borrow is there
+               // Also `to_digit` gives an Option, from which you must `unwrap` the 
+               // `Some` object. (also necessary typecast, potentially unsafe?)
+               new_board.push_str(&".".repeat(c.to_digit(10).unwrap() as usize));
+            // break if we get to the options, which should have a space afterwards
+            } else if c == ' ' {
+               break;
+            } else {
+               new_board.push(c);
+            }
+        }
+
+        // step 2: get active color
+        let player: Player;
+        if sections[1] == "w" {
+            player = Player::WHITE
+        } else {
+            player = Player::BLACK
+        }
+
+        // step 3: castling (i'm just gonna hardcode it)
+        let mut castling: u8 = 0;
+        // add (similar to bitwise or) for the available sides
+        if sections[2] != "-" {
+            for c in sections[2].chars() {
+                match c {
+                    'K' => castling += CastlingAvail::WhiteKingside as u8,
+                    'Q' => castling += CastlingAvail::WhiteQueenside as u8,
+                    'k' => castling += CastlingAvail::BlackKingside as u8,
+                    'q' => castling += CastlingAvail::BlackQueenside as u8,
+                    _ => castling += 0  // wildcard required
+                }
+            }
+        }
+
+        // step 4: en passant target
+        let ep_target: Position;
+        if sections[3] == "-" {
+            ep_target = Position{rank: 0, file: '_'}
+        } else {
+            let mut chars = sections[3].chars();
+            println!("{:?}a", chars);
+            ep_target = Position{
+                // gotta figure out how unwraps work, this seems superfluous
+                rank: chars.nth(1).unwrap().to_digit(10).unwrap() as u8, 
+                file: chars.nth(0).unwrap()
+            };
+        }
+
+        // step 5: halfmoves since last pawnpush or capture
+        let halfmove: u8 = sections[4].parse::<u8>().unwrap();
+
+        let fullmove: u8 = sections[5].parse::<u8>().unwrap();
+
+        return ChessState {
+            board: new_board,
+            active_player: player,
+            castling_avail: castling,
+            en_passant: ep_target,
+            halfmove_clock: halfmove,
+            fullmoves: fullmove
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,50 @@
-use regex::{Regex, Captures};
+mod chess;
 
-// outputs a prettyprinted board from a fen string
-fn fen_to_str(fen : String) -> String {
-    let mut board : String = String::new();
-
-    // step 0: regex to split the sections up
-    let re : Regex = Regex::new(r"[\w/-]+").unwrap();
-    // still not understanding the whole borrow thing, I guess it's due to being immutable?
-    let sections : Vec<&str> = re.captures_iter(&fen)
-        .map(|x: Captures| x.get(0).unwrap().as_str()) // Also map into string type
-        .collect();
-
-    // step 1: get boardstate
-    for c in sections[0].chars() {
-        if c == '/' {
-           board.push('\n');
-        } else if c.is_numeric() {
-           // Really wierd thing? Doesn't compile unless the borrow is there
-           // Also `to_digit` gives an Option, from which you must `unwrap` the 
-           // `Some` object. (also necessary typecast, potentially unsafe?)
-           board.push_str(&".".repeat(c.to_digit(10).unwrap() as usize));
-        // break if we get to the options, which should have a space afterwards
-        } else if c == ' ' {
-           break;
-        } else {
-           board.push(c);
-        }
-    }
-
-    // step 2: get active color
-    board += "\n\nActive Color: ";
-    if sections[1] == "w" {
-        board += "White"
-    } else {
-        board += "Black"
-    }
-
-    // step 3: castling (i'm just gonna hardcode it)
-    board += "\nCastling Availability:";
-    if sections[2].starts_with("KQ") {
-        board += "\n\tWhite can castle king- or queenside."
-    } else if sections[2].starts_with("K") {
-        board += "\n\tWhite can only castle kingside."
-    } else if sections[2].starts_with("Q") {
-        board += "\n\tWhite can only castle queenside."
-    }
-
-    if sections[2].ends_with("kq") {
-        board += "\n\tBlack can castle king- or queenside."
-    } else if sections[2].ends_with("k") {
-        board += "\n\tBlack can only castle kingside."
-    } else if sections[2].ends_with("q") {
-        board += "\n\tBlack can only castle queenside."
-    }
+// outputs a prettyprinted board and information from a ChessState object
+fn state_to_str(b: &chess::ChessState) -> String {
+    let mut castle: String = String::new();
     
-    if sections[2] == "-" {
-        board += " Neither side can castle."
-    }
-
-    // step 4: en passant target
-    board += "\nEn Passant Target: ";
-    if sections[3] == "-" {
-        board += "None"
+    // Format castling output via binary and
+    if b.castling_avail != 0 {
+        if b.castling_avail & chess::CastlingAvail::WhiteKingside != 0 {
+            castle += "\tWhite, Kingside\n"; 
+        }
+        if b.castling_avail & chess::CastlingAvail::WhiteQueenside != 0 {
+            castle += "\tWhite, Queenside\n";
+        }
+        if b.castling_avail & chess::CastlingAvail::BlackKingside != 0 {
+            castle += "\tBlack, Kingside\n"; 
+        }
+        if b.castling_avail & chess::CastlingAvail::BlackQueenside != 0 {
+            castle += "\tBlack, Queenside\n";
+        }
     } else {
-        board += sections[3]
+        castle += "\tNo castling available.\n";
     }
 
-    // step 5: halfmove clock (time since last pawnpush or capture)
-    board += "\nNo. of Halfmoves Since Last Pawnpush or Capture: ";
-    board += sections[4];
 
-    // step 6: fullmove clock (counts a move from white and one from black as one move)
-    board += "\nNo. of Fullmoves: ";
-    board += sections[5];
+    let out: String = format!("
+        {board}\n\n
+        Active Player: {active}\n
+        Castling Availability:\n{castling}
+        En Passant Target: {en_passant}\n
+        Halfmoves Since Last Pawnpush or Capture: {halfmoves}\n
+        No. of Fullmoves: {fullmoves}",
+        board=b.board,
+        active=b.active_player,
+        castling=b.castling_avail,
+        en_passant=b.en_passant,
+        halfmoves=b.halfmove_clock,
+        fullmoves=b.fullmoves
+    );
 
-    return board;
+    return out
 }
 
 fn main() {
     let fen = std::env::args().nth(1).expect("FEN string expected");
+    let board = chess::ChessState::new(&fen);
 
     // Gross? but println! only accepts literals OR doing it like this
-    println!("{}", fen_to_str(fen));
+    println!("{}", state_to_str(&board));
 }
